bf4623f775cb2ccab0e600fb4eba842b3948009d
torrigreen <tgreen14@tulane.edu>
2024-01-30 12:15:41 -0600

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(operator, num1, num2) {
+	return operator(num1, num2);
+}

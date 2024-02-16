8eebe3937fab385a36af9703090c543c89b4afd4
torrigreen <tgreen14@tulane.edu>
2024-02-16 14:26:48 -0600

    Create functions to manage display

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
 	return operator(num1, num2);
 }
+
+function updateDisplay(button) {
+	displayVal += button.textContent;
+	display.textContent = displayVal;
+}
+
+function clearDisplay() {
+	displayVal = '';
+	display.textContent = '';
+}
+
+const display = document.querySelector('.display');
+
+for (const button of document.querySelectorAll('button')) {
+	button.addEventListener('click', () => updateDisplay(button));
+}
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearDisplay);
+
+let firstNum;
+let secondNum;
+let operator;
+let displayVal = '';

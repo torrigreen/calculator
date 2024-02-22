7b03cd10d1766899f0658cc24de0e9379e2fe81a
torrigreen <tgreen14@tulane.edu>
2024-02-22 15:35:02 -0600

    Add input length limit

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function updateDisplay() {
 }
 
 function displayDigit(digit) {
+	if (displayVal.length > 8) return;
 	if (digit === '.' && displayVal.includes('.')) return;
 	displayVal += digit;
 	updateDisplay();
